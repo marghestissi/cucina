33b1e44ea3c707dc0f6e12719e9133f10dbf9308
A.Ahmed <ah.abdellatif96@gmail.com>
2020-02-18 14:50:17 +0100

    Commit 1 Iscrizione dao

--- src/dao/IscrizioneUtenteDAOImpl.java
+++ src/dao/IscrizioneUtenteDAOImpl.java
@@ -1,12 +1,18 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import com.mysql.fabric.xmlrpc.base.Array;
 
 import entity.Edizione;
 import entity.Utente;
 import exceptions.ConnessioneException;
+import exceptions.UtenteGiaRegistrato;
+import sun.nio.cs.ext.ISCII91;
 
 public class IscrizioneUtenteDAOImpl implements IscrizioneUtenteDAO {
 
@@ -22,9 +28,44 @@ public class IscrizioneUtenteDAOImpl implements IscrizioneUtenteDAO {
 	 * se l'utente e/o l'edizione non esistono o l'utente è già iscritto a quella edizione si solleva una eccezione
 	 */
 	@Override
-	public void iscriviUtente(int idEdizione, String idUtente) throws SQLException {
-		// TODO Auto-generated method stub
+	public void iscriviUtente(int idEdizione, String idUtente) {
 
+		Utente utente=null;
+		Edizione edizione=null;
+		try {
+			
+			RegistrazioneUtenteDAOImpl ur = new RegistrazioneUtenteDAOImpl();
+			 utente = ur.select(idUtente);
+			CalendarioDAOImpl calendario = new CalendarioDAOImpl();
+			edizione = calendario.selectEdizione(idEdizione);
+			
+
+			
+			
+			if(utente!=null && edizione !=null) {
+				ArrayList<Edizione> edizioniUtente = selectIscrizioniUtente(idUtente);
+				for(Edizione ed : edizioniUtente) {
+					if(idEdizione==ed.getCodice()) {
+						throw new UtenteGiaRegistrato();
+					}
+					
+				}
+				
+				PreparedStatement ps= conn.prepareStatement("INSERT INTO `iscritti` (`id_edizione`, `id_utente`) VALUES(?, ?)");
+				ps.setInt(1, idEdizione);
+				ps.setString(2, idUtente);
+				ps.executeUpdate();
+			}
+			
+		} catch (ConnessioneException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(UtenteGiaRegistrato e) {
+			e.printStackTrace();
+		} 
+		
 	}
 
 	/*
@@ -44,8 +85,32 @@ public class IscrizioneUtenteDAOImpl implements IscrizioneUtenteDAO {
 	 */
 	@Override
 	public ArrayList<Edizione> selectIscrizioniUtente(String idUtente) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<Edizione> edizioneUtente= new ArrayList<Edizione>();
+		PreparedStatement ps= conn.prepareStatement("select id_edizione from iscritti where id_utente=?");
+		ps.setString(1, idUtente);
+		
+		ResultSet rs = ps.executeQuery();
+		CalendarioDAOImpl calendario;
+		try {
+			calendario = new CalendarioDAOImpl();
+			while(rs.next()) {
+				
+				int i=rs.getInt(1);
+				edizioneUtente.add(calendario.selectEdizione(i));
+				
+			}
+		
+		} catch (ConnessioneException e) {
+	
+			e.printStackTrace();
+		}
+		
+		
+	
+		
+		
+		return edizioneUtente;
 	}
 	
 	/*
@@ -66,5 +131,11 @@ public class IscrizioneUtenteDAOImpl implements IscrizioneUtenteDAO {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	public static void main(String[] args) throws ConnessioneException {
+		IscrizioneUtenteDAOImpl in= new IscrizioneUtenteDAOImpl();
+		in.iscriviUtente(94,"Ing_Ruben");
+	}
+
 
 }
---
+++ src/exceptions/UtenteGiaRegistrato.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class UtenteGiaRegistrato extends Exception{
+
+}
